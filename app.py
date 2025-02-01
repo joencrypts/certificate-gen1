06ad9b750b67601e0c90f62ac5944775ff9b35a3
Shanjo J Benadict <117262505+joencrypts@users.noreply.github.com>
2025-02-01 23:51:13 +0530

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def process():
     draw = ImageDraw.Draw(img)
 
     try:
-        font = ImageFont.truetype("static/fonts/TimesNewRoman.ttf", size=50)
+        font = ImageFont.truetype("static/fonts/TimesNewRoman.ttf", size=45)
 
     except IOError:
         font = ImageFont.load_default()  
